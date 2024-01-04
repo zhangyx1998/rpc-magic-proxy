b78464eacc4fb0b773309914a860ad5c2c873ac1
Yuxuan Zhang <github@z-yx.cc>
2024-01-04 04:48:21 -0500

    examples: add "multi-jump"

---
+++ examples/multi-jump.mjs
@@ -0,0 +1,35 @@
+import { Worker, isMainThread, parentPort, workerData } from "worker_threads";
+import RpcContext, { deferPromise } from "rpc-magic-proxy";
+
+async function main() {
+  const ctx = new RpcContext();
+  const { promise, resolve } = deferPromise();
+  // First worker
+  ctx.bind(new Worker(new URL(import.meta.url), {
+    workerData: await ctx.serialize({ resolve })
+  }));
+  // Second worker
+  ctx.bind(new Worker(new URL(import.meta.url), {
+    workerData: await ctx.serialize({ callback: await promise })
+  }));
+}
+
+async function worker() {
+  const ctx = new RpcContext(parentPort);
+  console.log("workerData:", workerData)
+  const { callback, resolve } = ctx.deserialize(workerData);
+  // First worker
+  if (resolve) {
+    const { promise, resolve: _resolve } = deferPromise()
+    await resolve(_resolve)
+    console.log("Worker 1 got:", await promise)
+  }
+  // Second worker
+  if (callback) {
+    await callback("Hello from worker 2")
+  }
+  // This will unbind listeners and allow worker to exit
+  ctx.reset();
+}
+
+isMainThread ? main() : worker()
